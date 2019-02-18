d7f5ed72610f978f18859bc01444cfa75e4be74b
Bana8 <banafsheh@Banafshehs-MacBook-Pro.local>
2019-02-18 09:11:10 -0500

    small modificationsFeb2019

--- iOS/mainViewController.designer.cs
+++ iOS/mainViewController.designer.cs
@@ -1,62 +1,67 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace OESApplication.iOS
 {
-	[Register ("mainViewController")]
-	partial class mainViewController
-	{
-		[Outlet]
-		UIKit.UIButton captureSpectraButton { get; set; }
+    [Register ("mainViewController")]
+    partial class mainViewController
+    {
+        [Outlet]
+        UIKit.UIButton captureSpectraButton { get; set; }
 
-		[Outlet]
-		UIKit.UIImageView CrossHair { get; set; }
 
-		[Outlet]
-		UIKit.UIImageView ImagePreview { get; set; }
+        [Outlet]
+        UIKit.UIImageView CrossHair { get; set; }
 
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		UIKit.UIView liveCameraStream { get; set; }
 
-		[Outlet]
-		UIKit.UIButton takeimageButton { get; set; }
+        [Outlet]
+        UIKit.UIImageView ImagePreview { get; set; }
 
-		[Action ("captureSpectraTouchUpInside:")]
-		partial void captureSpectraTouchUpInside (Foundation.NSObject sender);
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (ImagePreview != null) {
-				ImagePreview.Dispose ();
-				ImagePreview = null;
-			}
 
-			if (captureSpectraButton != null) {
-				captureSpectraButton.Dispose ();
-				captureSpectraButton = null;
-			}
+        [Outlet]
+        UIKit.UIButton takeimageButton { get; set; }
 
-			if (CrossHair != null) {
-				CrossHair.Dispose ();
-				CrossHair = null;
-			}
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView liveCameraStream { get; set; }
 
-			if (liveCameraStream != null) {
-				liveCameraStream.Dispose ();
-				liveCameraStream = null;
-			}
 
-			if (takeimageButton != null) {
-				takeimageButton.Dispose ();
-				takeimageButton = null;
-			}
-		}
-	}
+        [Action ("captureSpectraTouchUpInside:")]
+        partial void captureSpectraTouchUpInside (Foundation.NSObject sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (captureSpectraButton != null) {
+                captureSpectraButton.Dispose ();
+                captureSpectraButton = null;
+            }
+
+            if (CrossHair != null) {
+                CrossHair.Dispose ();
+                CrossHair = null;
+            }
+
+            if (ImagePreview != null) {
+                ImagePreview.Dispose ();
+                ImagePreview = null;
+            }
+
+            if (liveCameraStream != null) {
+                liveCameraStream.Dispose ();
+                liveCameraStream = null;
+            }
+
+            if (takeimageButton != null) {
+                takeimageButton.Dispose ();
+                takeimageButton = null;
+            }
+        }
+    }
 }
