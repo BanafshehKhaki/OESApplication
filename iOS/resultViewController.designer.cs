11c9c76e36b196846a9cb13409541c115f434ac8
Bana8 <banafsheh@Banafshehs-MacBook-Pro.local>
2019-11-17 13:23:28 -0500

    Absorbance result File now saved in folders.Also, BlnkControl button created

--- iOS/resultViewController.designer.cs
+++ iOS/resultViewController.designer.cs
@@ -1,47 +1,75 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
 
 namespace OESApplication.iOS
 {
-    [Register ("resultViewController")]
-    partial class resultViewController
-    {
-        [Outlet]
-        UIKit.UIImageView detectedSpectra { get; set; }
+	[Register ("resultViewController")]
+	partial class resultViewController
+	{
+		[Outlet]
+		UIKit.UIButton blankControl { get; set; }
 
+		[Outlet]
+		UIKit.UIImageView detectedSpectra { get; set; }
 
-        [Outlet]
-        UIKit.UIButton measureNitrate { get; set; }
+		[Outlet]
+		UIKit.UIButton measureNitrate { get; set; }
 
+		[Outlet]
+		UIKit.UIButton measurePH { get; set; }
 
-        [Outlet]
-        UIKit.UIButton measurePH { get; set; }
+		[Outlet]
+		UIKit.UILabel resultOutput { get; set; }
 
+		[Outlet]
+		UIKit.UITextField resultOutputBox { get; set; }
 
-        [Outlet]
-        UIKit.UILabel resultOutput { get; set; }
+		[Action ("measureNitrateTouchUpInside:")]
+		partial void measureNitrateTouchUpInside (Foundation.NSObject sender);
 
+		[Action ("measurePHTouchUpInside:")]
+		partial void measurePHTouchUpInside (Foundation.NSObject sender);
 
-        [Outlet]
-        UIKit.UIStackView StackView { get; set; }
+		[Action ("setBlankControlTouchUpInside:")]
+		partial void setBlankControlTouchUpInside (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (blankControl != null) {
+				blankControl.Dispose ();
+				blankControl = null;
+			}
 
+			if (detectedSpectra != null) {
+				detectedSpectra.Dispose ();
+				detectedSpectra = null;
+			}
 
-        [Action ("measureNitrateTouchUpInside:")]
-        partial void measureNitrateTouchUpInside (Foundation.NSObject sender);
+			if (measureNitrate != null) {
+				measureNitrate.Dispose ();
+				measureNitrate = null;
+			}
 
+			if (measurePH != null) {
+				measurePH.Dispose ();
+				measurePH = null;
+			}
 
-        [Action ("measurePHTouchUpInside:")]
-        partial void measurePHTouchUpInside (Foundation.NSObject sender);
+			if (resultOutput != null) {
+				resultOutput.Dispose ();
+				resultOutput = null;
+			}
 
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+			if (resultOutputBox != null) {
+				resultOutputBox.Dispose ();
+				resultOutputBox = null;
+			}
+		}
+	}
 }
